673162f36da82b34feafe75fd0b54eacabd605e2
Guillermo <nokamihakai@gmail.com>
2023-08-14 08:36:06 -0500

    menu despues del login

---
+++ menu_inicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="css/menu_inicio.css" rel="stylesheet" />
+        <title>INICIO 19D</title>
+    </head>
+    <body>
+      <ul class="slider">
+        <li id="slide1" class="nav-item">
+          <a class="nav-link" href="empresa_form.php">NOSOTROS</a>
+        </li>
+        <li id="slide2" class="nav-item">
+          <a class="nav-link" href="servicio_form.php">SERVICIOS</a>
+        </li>
+        <li id="slide3" class="nav-item">
+          <a class="nav-link" href="desarrollo_form.php">DESARROLLOS</a>
+        </li>
+      </ul>
+      <nav>
+        <ul class="menu">
+          <li><a href="#slide1">Nosotros</a></li>
+          <li><a href="#slide2">Servicios</a></li>
+          <li><a href="#slide3">Desarrollos</a></li>
+        </ul>
+      </nav>
+    </body>
+</html>
