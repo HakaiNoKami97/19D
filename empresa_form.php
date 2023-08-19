0b0b15cb2a048f8949297a2bb8a8bc7075b18f9e
Guillermo <nokamihakai@gmail.com>
2023-08-19 11:50:20 -0500

    Dashboard

--- empresa_form.php
+++ empresa_form.php
@@ -2,16 +2,72 @@
 <html>
 <head>
     <link href="css/styleempresa.css" rel="stylesheet" />
+    <link href="css/dashboard.css" rel="stylesheet" />
     <title>NOSOTROS 19D</title>
 </head>
 <body>
-    <h2>NOSOTROS</h2>
-    
-    <!-- Formulario para agregar/editar registros -->
-    <form action="empresa_process.php" method="post">
-        <?php
-        if (isset($_GET['edit_id'])) {
-            $edit_id = $_GET['edit_id'];
+    <div class="dashboard">
+        <div class="sidebar">
+            <ul>
+                <li><a href="menu_inicio.php">Inicio</a></li>
+                <li><a href="empresa_form.php">Nosotros</a></li>
+                <li><a href="servicio_form.php">Registrar Servicio</a></li>                
+                <li><a href="desarrollo_form.php">Registrar Desarrollo</a></li>
+            </ul>
+        </div>
+    <div class="content">
+        <h2>NOSOTROS</h2>
+        
+        <!-- Formulario para agregar/editar registros -->
+        <form action="empresa_process.php" method="post">
+            <?php
+            if (isset($_GET['edit_id'])) {
+                $edit_id = $_GET['edit_id'];
+                $servername = "localhost:3306";
+                $username = "root";
+                $password = "";
+                $dbname = "administrador";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if ($conn->connect_error) {
+                    die("Error al conectarse a la base de datos: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT * FROM empresa WHERE id = $edit_id";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $misionValue = $row['misión'];
+                    $visionValue = $row['visión'];
+                    echo 'Misión: <textarea name="mision" rows="3" required>' . $misionValue . '</textarea><br>';
+                    echo 'Visión: <textarea name="vision" rows="3" required>' . $visionValue . '</textarea><br>';
+                    echo '<input type="hidden" name="edit_id" value="' . $edit_id . '">';
+                    echo '<input type="submit" name="update" value="Actualizar">';
+                } else {
+                    echo "No se encontraron datos para editar.";
+                }
+
+                $conn->close();
+            } else {
+                // Si no estamos editando, mostrar campos vacíos
+                echo 'Misión: <textarea name="mision" rows="3" required></textarea><br>';
+                echo 'Visión: <textarea name="vision" rows="3" required></textarea><br>';
+                echo '<input type="submit" name="save" value="Guardar">';
+            }
+            ?>
+        </form>
+
+        <h2>Sobre Nosotros</h2>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Misión</th>
+                <th>Visión</th>
+            </tr>
+            
+            <?php
             $servername = "localhost:3306";
             $username = "root";
             $password = "";
@@ -23,71 +79,28 @@
                 die("Error al conectarse a la base de datos: " . $conn->connect_error);
             }
 
-            $sql = "SELECT * FROM empresa WHERE id = $edit_id";
+            // Realizar consulta a la tabla "empresa"
+            $sql = "SELECT * FROM empresa";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $misionValue = $row['misión'];
-                $visionValue = $row['visión'];
-                echo 'Misión: <textarea name="mision" rows="3" required>' . $misionValue . '</textarea><br>';
-                echo 'Visión: <textarea name="vision" rows="3" required>' . $visionValue . '</textarea><br>';
-                echo '<input type="hidden" name="edit_id" value="' . $edit_id . '">';
-                echo '<input type="submit" name="update" value="Actualizar">';
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['misión'] . "</td>";
+                    echo "<td>" . $row['visión'] . "</td>";
+                    echo '<td><a href="empresa_form.php?edit_id=' . $row['id'] . '">Editar</a></td>';
+                    echo '<td><a href="empresa_process.php?delete_id=' . $row['id'] . '">Eliminar</a></td>';
+                    echo "</tr>";
+                }
             } else {
-                echo "No se encontraron datos para editar.";
+                echo "<tr><td colspan='3'>No hay registros disponibles</td></tr>";
             }
 
             $conn->close();
-        } else {
-            // Si no estamos editando, mostrar campos vacíos
-            echo 'Misión: <textarea name="mision" rows="3" required></textarea><br>';
-            echo 'Visión: <textarea name="vision" rows="3" required></textarea><br>';
-            echo '<input type="submit" name="save" value="Guardar">';
-        }
-        ?>
-    </form>
-
-    <h2>Sobre Nosotros</h2>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Misión</th>
-            <th>Visión</th>
-        </tr>
-        
-        <?php
-        $servername = "localhost:3306";
-        $username = "root";
-        $password = "";
-        $dbname = "administrador";
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        if ($conn->connect_error) {
-            die("Error al conectarse a la base de datos: " . $conn->connect_error);
-        }
-
-        // Realizar consulta a la tabla "empresa"
-        $sql = "SELECT * FROM empresa";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['misión'] . "</td>";
-                echo "<td>" . $row['visión'] . "</td>";
-                echo '<td><a href="empresa_form.php?edit_id=' . $row['id'] . '">Editar</a></td>';
-                echo '<td><a href="empresa_process.php?delete_id=' . $row['id'] . '">Eliminar</a></td>';
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='3'>No hay registros disponibles</td></tr>";
-        }
-
-        $conn->close();
-        ?>
-    </table>
+            ?>
+        </table>
+    </div>
+    </div>
 </body>
 </html>
--- servicio_form.php
+++ servicio_form.php
@@ -2,17 +2,81 @@
 <html>
 <head>
     <link href="css/styleservicio.css" rel="stylesheet" />
+    <link href="css/dashboard.css" rel="stylesheet" />
     <title>SERVICIOS 19D</title>
 </head>
 <body>
-    <div class="container">
-        <h2>Registrar Servicio</h2>
-        
-        <!-- Formulario para agregar/editar registros -->
-        <form action="servicio_process.php" method="post" enctype="multipart/form-data">
-            <?php
-            if (isset($_GET['edit_id'])) {
-                $edit_id = $_GET['edit_id'];
+    <div class="dashboard">
+        <div class="sidebar">
+            <ul>
+                <li><a href="menu_inicio.php">Inicio</a></li>
+                <li><a href="empresa_form.php">Nosotros</a></li>
+                <li><a href="servicio_form.php">Registrar Servicio</a></li>                
+                <li><a href="desarrollo_form.php">Registrar Desarrollo</a></li>
+            </ul>
+        </div>
+    <div class="content">
+        <div class="container">
+            <h2>Registrar Servicio</h2>
+            
+            <!-- Formulario para agregar/editar registros -->
+            <form action="servicio_process.php" method="post" enctype="multipart/form-data">
+                <?php
+                if (isset($_GET['edit_id'])) {
+                    $edit_id = $_GET['edit_id'];
+                    $servername = "localhost:3306";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "administrador";
+
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+
+                    if ($conn->connect_error) {
+                        die("Error al conectarse a la base de datos: " . $conn->connect_error);
+                    }
+
+                    $sql = "SELECT * FROM servicio WHERE id = $edit_id";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        echo "<h3>Editar Servicio</h3>";
+                        echo '<form action="servicio_process.php" method="post" enctype="multipart/form-data">';
+                        echo 'Nombre: <input type="text" name="nombre" value="' . $row['nombre'] . '" required><br>';
+                        echo 'Descripción: <textarea name="descripcion" rows="3" required>' . $row['descripción'] . '</textarea><br>';
+                        echo 'Imagen Actual: <img src="' . $row['imagen']. '" alt="Imagen del Servicio" width="100"><br>';
+                        echo 'Nueva Imagen: <input type="file" name="imagen" accept="image/*"><br>';
+                        echo '<input type="hidden" name="edit_id" value="' . $edit_id . '">';
+                        echo '<input type="submit" name="update" value="Actualizar">';
+                        echo '</form>';
+                    } else {
+                        echo "No se encontraron datos para editar.";
+                    }
+
+                    $conn->close();
+                } else {
+                    // Si no estamos editando, mostrar campos vacíos
+                    echo 'Nombre: <input type="text" name="nombre" required><br>';
+                    echo 'Descripción: <textarea name="descripcion" rows="3" required></textarea><br>';
+                    echo 'Imagen: <input type="file" name="imagen" accept="image/*"><br>';
+                    echo '<input type="submit" name="save" value="Guardar">';
+                }
+                ?>
+            </form>
+
+            <h2>Sobre Nosotros</h2>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>TITULO</th>
+                    <th>DESCRIPCIÓN</th>
+                    <th>IMAGEN</th>
+                    <th>FECHA</th>
+                    <th>EDITAR</th>
+                    <th>ELIMINAR</th>
+                </tr>
+                
+                <?php
                 $servername = "localhost:3306";
                 $username = "root";
                 $password = "";
@@ -24,83 +88,31 @@
                     die("Error al conectarse a la base de datos: " . $conn->connect_error);
                 }
 
-                $sql = "SELECT * FROM servicio WHERE id = $edit_id";
+                // Realizar consulta a la tabla "empresa"
+                $sql = "SELECT * FROM servicio";
                 $result = $conn->query($sql);
 
                 if ($result->num_rows > 0) {
-                    $row = $result->fetch_assoc();
-                    echo "<h3>Editar Servicio</h3>";
-                    echo '<form action="servicio_process.php" method="post" enctype="multipart/form-data">';
-                    echo 'Nombre: <input type="text" name="nombre" value="' . $row['nombre'] . '" required><br>';
-                    echo 'Descripción: <textarea name="descripcion" rows="3" required>' . $row['descripción'] . '</textarea><br>';
-                    echo 'Imagen Actual: <img src="' . $row['imagen']. '" alt="Imagen del Servicio" width="100"><br>';
-                    echo 'Nueva Imagen: <input type="file" name="imagen" accept="image/*"><br>';
-                    echo '<input type="hidden" name="edit_id" value="' . $edit_id . '">';
-                    echo '<input type="submit" name="update" value="Actualizar">';
-                    echo '</form>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['id'] . "</td>";
+                        echo "<td>" . $row['nombre'] . "</td>";
+                        echo "<td>" . $row['descripción'] . "</td>";
+                        echo "<td><img src='" . $row['imagen'] . "' alt='Imagen de Servicio' width='100'></td>";
+                        echo "<td>" . $row['fecha'] . "</td>";
+                        echo '<td><a href="servicio_form.php?edit_id=' . $row['id'] . '">Editar</a></td>';
+                        echo '<td><a href="servicio_process.php?delete_id=' . $row['id'] . '">Eliminar</a></td>';
+                        echo "</tr>";
+                    }
                 } else {
-                    echo "No se encontraron datos para editar.";
+                    echo "<tr><td colspan='3'>No hay registros disponibles</td></tr>";
                 }
 
                 $conn->close();
-            } else {
-                // Si no estamos editando, mostrar campos vacíos
-                echo 'Nombre: <input type="text" name="nombre" required><br>';
-                echo 'Descripción: <textarea name="descripcion" rows="3" required></textarea><br>';
-                echo 'Imagen: <input type="file" name="imagen" accept="image/*"><br>';
-                echo '<input type="submit" name="save" value="Guardar">';
-            }
-            ?>
-        </form>
-
-        <h2>Sobre Nosotros</h2>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>TITULO</th>
-                <th>DESCRIPCIÓN</th>
-                <th>IMAGEN</th>
-                <th>FECHA</th>
-                <th>EDITAR</th>
-                <th>ELIMINAR</th>
-            </tr>
-            
-            <?php
-            $servername = "localhost:3306";
-            $username = "root";
-            $password = "";
-            $dbname = "administrador";
-
-            $conn = new mysqli($servername, $username, $password, $dbname);
-
-            if ($conn->connect_error) {
-                die("Error al conectarse a la base de datos: " . $conn->connect_error);
-            }
-
-            // Realizar consulta a la tabla "empresa"
-            $sql = "SELECT * FROM servicio";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['id'] . "</td>";
-                    echo "<td>" . $row['nombre'] . "</td>";
-                    echo "<td>" . $row['descripción'] . "</td>";
-                    echo "<td><img src='" . $row['imagen'] . "' alt='Imagen de Servicio' width='100'></td>";
-                    echo "<td>" . $row['fecha'] . "</td>";
-                    echo '<td><a href="servicio_form.php?edit_id=' . $row['id'] . '">Editar</a></td>';
-                    echo '<td><a href="servicio_process.php?delete_id=' . $row['id'] . '">Eliminar</a></td>';
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr><td colspan='3'>No hay registros disponibles</td></tr>";
-            }
-
-            $conn->close();
-            ?>
-        </table>
+                ?>
+            </table>
+        </div>
     </div>
-    
+    </div>
 </body>
 </html>
