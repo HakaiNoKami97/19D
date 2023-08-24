4e40ac3278f49d4e2a70f38f7f4ed4491523f135
Guillermo <nokamihakai@gmail.com>
2023-08-24 08:22:23 -0500

    correcciones

--- desarrollo_process.php
+++ desarrollo_process.php
@@ -37,7 +37,13 @@
             exit();
         }
 
-        // Resto del código...
+        // Subir imagen al servidor
+        $imagen_ruta = "uploads/" . $imagen_nombre;
+        move_uploaded_file($imagen_tmp, $imagen_ruta);
+
+        // Insertar datos en la base de datos
+        $sql = "INSERT INTO desarrollo (nombre, descripción, imagen, fecha)
+                VALUES ('$nombre', '$descripcion', '$imagen_ruta', '$fecha_registro')";
 
         if ($conn->query($sql) === TRUE) {
             echo "<script>
@@ -53,36 +59,76 @@
                   </script>";
         }
     } elseif (isset($_POST['update'])) {
-        // Resto del código...
+        $edit_id = $_POST['edit_id'];
+        $nombre = trim($_POST['nombre']);
+        $descripcion = trim($_POST['descripcion']);
+
+        if (empty($nombre) || empty($descripcion)) {
+            echo "<script>
+                    Swal.fire('Advertencia', 'Por favor, completa todos los campos antes de enviar el formulario.', 'warning').then(function() {
+                        window.location.href = 'desarrollo_form.php';
+                    });
+                </script>";
+            exit();
+        }
+
+        // Verificar si se cargó una nueva imagen
+        if ($_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
+            $imagen_nombre = $_FILES['imagen']['name'];
+            $imagen_tmp = $_FILES['imagen']['tmp_name'];
+
+            // Ruta donde se guardarán las imágenes
+            $imagen_ruta = "uploads/" . $imagen_nombre;
+
+            // Subir nueva imagen al servidor
+            move_uploaded_file($imagen_tmp, $imagen_ruta);
+
+            // Actualizar detalles del servicio en la base de datos, incluyendo la imagen
+            $sql = "UPDATE desarrollo SET
+                    nombre = '$nombre',
+                    descripción = '$descripcion',
+                    imagen = '$imagen_ruta'
+                    WHERE id = $edit_id";
+
+        } else {
+            // Actualizar detalles del servicio en la base de datos sin cambiar la imagen
+            $sql = "UPDATE desarrollo SET
+                    nombre = '$nombre',
+                    descripción = '$descripcion'
+                    WHERE id = $edit_id";
+        }
 
         if ($conn->query($sql) === TRUE) {
             echo "<script>
                     Swal.fire('Éxito', 'Actualización de desarrollo exitosa', 'success').then(function() {
                         window.location.href = 'desarrollo_form.php';
                     });
-                  </script>";
+                </script>";
         } else {
             echo "<script>
                     Swal.fire('Error', 'Error al actualizar desarrollo: " . $conn->error . "', 'error').then(function() {
                         window.location.href = 'desarrollo_form.php';
                     });
-                  </script>";
+                </script>";
         }
     } elseif (isset($_GET['delete_id'])) {
-        // Resto del código...
+        $delete_id = $_GET['delete_id'];
+
+        // Realizar la eliminación en la base de datos
+        $sql = "DELETE FROM desarrollo WHERE id = $delete_id";
 
         if ($conn->query($sql) === TRUE) {
             echo "<script>
                     Swal.fire('Éxito', 'Registro eliminado exitosamente', 'success').then(function() {
                         window.location.href = 'desarrollo_form.php';
                     });
-                  </script>";
+                </script>";
         } else {
             echo "<script>
                     Swal.fire('Error', 'Error al eliminar registro: " . $conn->error . "', 'error').then(function() {
                         window.location.href = 'desarrollo_form.php';
                     });
-                  </script>";
+                </script>";
         }
     }
 
